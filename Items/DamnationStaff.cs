12af047ed76654abd935ba50dbd1eb9da6c10626
ColinAV516 <colinav516@gmail.com>
2016-12-31 01:22:05 -0500

    Damnation staff

---
+++ Items/DamnationStaff.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items
+{
+    public class DamnationStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Damnation Staff";
+            item.damage = 45;
+            item.magic = true;
+            item.mana = 8;
+            item.width = 25;
+            item.height = 26;
+            item.toolTip = "Fires an exploding orb";
+            item.useTime = 16;
+			item.UseSound = SoundID.Item20;
+            item.useAnimation = 16;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 7;
+            Item.staff[item.type] = true;
+            item.value = 10000;
+            item.rare = 2;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("hellorb");
+            item.shootSpeed = 10f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HellstoneBar, 15);
+			recipe.AddIngredient(ItemID.SoulofNight, 10);
+			recipe.AddIngredient(ItemID.SoulofFright, 10);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+        }
+	}
+}
---
+++ Projectiles/hellorb.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System;
+
+namespace ForgottenMemories.Projectiles
+{
+    public class hellorb : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Hell Orb";
+            projectile.width = 22;
+            projectile.height = 22;
+            projectile.aiStyle = 2;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 200;
+			projectile.extraUpdates = 3;
+        }
+		
+		public override void AI()
+		{
+			if (Main.rand.Next(2) == 0)
+			{
+				int dust;
+				dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 6, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+				Main.dust[dust].scale = 1.5f;
+				Main.dust[dust].noGravity = true;
+			}
+		}
+		
+		public override void Kill(int timeLeft)
+			{
+				Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, mod.ProjectileType("hellboom"), 50, 5f, projectile.owner);
+				 Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 62);
+			}
+        }
+    }
