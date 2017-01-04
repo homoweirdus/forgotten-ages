465a2bbb0d0e4efad1e71c701e77f5c15f34d8e1
ColinAV516 <colinav516@gmail.com>
2017-01-04 03:00:26 -0500

    Slime rod
    slime rod fofsd

--- Items/SlimeRod.cs
+++ Items/SlimeRod.cs
@@ -6,20 +6,20 @@ using Terraria.ModLoader;
 
 namespace ForgottenMemories.Items
 {
-    public class DamnationStaff : ModItem
+    public class SlimeRod : ModItem
     {
         public override void SetDefaults()
         {
-            item.name = "Damnation Staff";
-            item.damage = 33;
+            item.name = "Slime Rod";
+            item.damage = 15;
             item.magic = true;
             item.mana = 8;
             item.width = 25;
             item.height = 26;
-            item.toolTip = "Fires an exploding orb";
-            item.useTime = 13;
+            item.toolTip = "Fires slime balls";
+            item.useTime = 3;
 			item.UseSound = SoundID.Item20;
-            item.useAnimation = 13;
+            item.useAnimation = 12;
             item.useStyle = 5;
             item.noMelee = true;
             item.knockBack = 7;
@@ -27,8 +27,9 @@ namespace ForgottenMemories.Items
             item.value = 10000;
             item.rare = 2;
             item.autoReuse = true;
-            item.shoot = mod.ProjectileType("hellorb");
-            item.shootSpeed = 16f;
+            item.shoot = mod.ProjectileType("SlimeBall");
+            item.shootSpeed = 7f;
+			item.reuseDelay = 30;
         }
 		
 		
@@ -36,8 +37,8 @@ namespace ForgottenMemories.Items
     {
             float sX = speedX;
             float sY = speedY;
-            sX += (float)Main.rand.Next(-60, 61) * 0.02f;
-            sY += (float)Main.rand.Next(-60, 61) * 0.02f;
+            sX += (float)Main.rand.Next(-60, 61) * 0.05f;
+            sY += (float)Main.rand.Next(-60, 61) * 0.05f;
             Projectile.NewProjectile(position.X, position.Y, sX, sY, type, damage, knockBack, player.whoAmI);
 			return false;
     }
@@ -45,10 +46,9 @@ namespace ForgottenMemories.Items
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.HellstoneBar, 15);
-			recipe.AddIngredient(ItemID.SoulofNight, 10);
-			recipe.AddIngredient(ItemID.SoulofFright, 10);
-            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.AddIngredient(null, "ChampionCrystal", 5);
+			recipe.AddIngredient(ItemID.Gel, 100);
+            recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
 
