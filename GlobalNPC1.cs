a5c7a6143c849fb2f8cde99e81386fd8a6c5c2ea
homoweirdus <cpm2012@sbcglobal.net>
2017-02-04 22:17:55 -0500

    eftrgvbedfgg

---
+++ GlobalNPC1.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories
+{
+	public class GlobalNPC1 : GlobalNPC
+	{
+		
+		
+
+		public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+		{
+			if (player.GetModPlayer<MyPlayer>(mod).isGlitch)
+			{
+				spawnRate = (int)(spawnRate * 50f);
+				maxSpawns = (int)(maxSpawns * 50f);
+			}
+		}
+
+	
+	}
+}
