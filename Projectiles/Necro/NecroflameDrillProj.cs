0675470bdfd284f67110e9f480559ca5c643b0eb
ColinAV516 <colinav516@gmail.com>
2017-02-06 18:42:27 -0500

    wtf tho

--- Projectiles/Necro/NecroflameDrillProj.cs
+++ Projectiles/Necro/NecroflameDrillProj.cs
@@ -4,7 +4,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace TGEM.Projectiles.Melee
+namespace ForgottenMemories.Projectiles.Necro
 {
 	
 	public class NecroflameDrillProj : ModProjectile
