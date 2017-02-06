9cff9a869c0773dbcc9e3fc7d36591afdaa9ab26
homoweirdus <cpm2012@sbcglobal.net>
2017-02-06 17:18:47 -0500

    I DID IT STFU COLIN

---
+++ Projectiles/OldProjectiles/OldBoom.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace ForgottenMemories.Projectiles.OldProjectiles
+{
+    public class OldBoom : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "OldBoom";
+            projectile.width = 30;
+            projectile.height = 30;
+            projectile.aiStyle = 3;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.penetrate = 3;
+            projectile.timeLeft = 600;
+            projectile.light = 0.5f;
+            projectile.extraUpdates = 1;
+           
+           
+        }
+       
+ 
+    }
+}
