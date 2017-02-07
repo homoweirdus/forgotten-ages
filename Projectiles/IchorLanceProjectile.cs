1be893fe8ebd75dd723a0c323192a9e8551f38d6
Kachow911 <torniky@gmail.com>
2017-02-06 18:51:13 -0800

    lances and sheet

---
+++ Projectiles/IchorLanceProjectile.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+    public class IchorLanceProjectile : ModProjectile
+    {
+    	
+        public override void SetDefaults()
+        {
+            projectile.name = "Ichor Lance";
+			projectile.width = 40;  
+			projectile.aiStyle = 19;
+			projectile.melee = true; 
+			projectile.timeLeft = 90;
+			projectile.height = 40;  
+			projectile.friendly = true;
+			projectile.hostile = false;
+			projectile.tileCollide = false;
+			projectile.ignoreWater = true;
+			projectile.penetrate = -1;
+			projectile.ownerHitCheck = true;
+			projectile.hide = true;
+        }
+
+        public override void AI()
+        {
+        	Main.player[projectile.owner].direction = projectile.direction;
+        	Main.player[projectile.owner].heldProj = projectile.whoAmI;
+        	Main.player[projectile.owner].itemTime = Main.player[projectile.owner].itemAnimation;
+        	projectile.position.X = Main.player[projectile.owner].position.X + (float)(Main.player[projectile.owner].width / 2) - (float)(projectile.width / 2);
+        	projectile.position.Y = Main.player[projectile.owner].position.Y + (float)(Main.player[projectile.owner].height / 2) - (float)(projectile.height / 2);
+        	projectile.position += projectile.velocity * projectile.ai[0];
+        	if(projectile.ai[0] == 0f)
+        	{
+        		projectile.ai[0] = 3f;
+        		projectile.netUpdate = true;
+        	}
+        	if(Main.player[projectile.owner].itemAnimation < Main.player[projectile.owner].itemAnimationMax / 3)
+        	{
+        		projectile.ai[0] -= 2.4f;
+				if (projectile.localAI[0] == 0f && Main.myPlayer == projectile.owner)
+				{
+					projectile.localAI[0] = 1f;
+					Projectile.NewProjectile(projectile.Center.X + (projectile.velocity.X * 3/4), projectile.Center.Y + (projectile.velocity.Y * 3/4), projectile.velocity.X * 1.4f, projectile.velocity.Y * 1.4f, mod.ProjectileType("IchorSpearTip"), (int)((double)projectile.damage * 0.85f), projectile.knockBack * 0.85f, projectile.owner, 0f, 0f);
+				}
+        	}
+        	else
+        	{
+        		projectile.ai[0] += 0.95f;
+        	}
+        	
+        	if(Main.player[projectile.owner].itemAnimation == 0)
+        	{
+        		projectile.Kill();
+        	}
+        	
+        	projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 2.355f;
+        	if(projectile.spriteDirection == -1)
+        	{
+        		projectile.rotation -= 1.57f;
+        	}
+        }
+    }
+}
