27360124aadf74d66929a8230b479090c85e6578
zoroarkcity <zoroarkcity2002@gmail.com>
2017-02-09 16:11:10 -0800

    Slimethrower

---
+++ Items/ItemSets/Gelatine/Slimethrower.cs
@@ -0,0 +1,48 @@
+using Terraria;
+using System;
+using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.ItemSets.Gelatine
+{
+	public class Slimethrower : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Slimethrower";
+			item.damage = 13;
+			item.ranged = true;
+			item.width = 23;
+			item.height = 13;
+			item.toolTip = "Shoots high velocity streams of gel";
+			item.useTime = 6;
+			item.useAnimation = 30;
+			item.useStyle = 5;
+			item.noMelee = true;
+			item.knockBack = 4;
+			item.value = 40000;
+			item.rare = 1;
+			item.UseSound = SoundID.Item34;
+			item.autoReuse = true;
+			item.shoot = mod.ProjectileType("gelstream");
+			item.shootSpeed = 5.25f;
+			item.useAmmo = AmmoID.Gel;
+		}
+		
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-3, 0);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "GelatineBar", 12);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
