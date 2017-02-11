fe01c9c03251d9dbb03965ac9ade84763c9366e9
ColinAV516 <colinav516@gmail.com>
2017-02-11 14:56:42 -0500

    kill yourself

---
+++ Items/ItemSets/Gelatine/SlimyNecklace.cs
@@ -0,0 +1,35 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.ItemSets.Gelatine 
+{
+public class SlimyNecklace : ModItem
+{
+    public override void SetDefaults()
+    {
+		item.name = "Slimy Necklace";
+        item.width = 24;
+        item.height = 28;
+        item.toolTip = "Minions have a chance to slow down enemies";
+        item.value = 40000;
+        item.rare = 1;
+        item.accessory = true;
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+	{
+        ((EnergyPlayer)player.GetModPlayer(mod, "EnergyPlayer")).SlimyNeck = true;
+	}
+	
+	public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "GelatineBar", 12);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+}
+}
