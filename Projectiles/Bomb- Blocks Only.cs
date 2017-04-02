330ee9945f792d7dd69bfd926fde138603f3b9cc
Zekrom <ibrahimkhawaja08@gmail.com>
2017-04-01 22:39:37 -0500

    Bomb that only kills blocks, good for building/deconstructing
    Sieg Heil?

---
+++ Projectiles/Bomb- Blocks Only.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+
+{
+    public class BombBlocksOnly : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(166);
+            projectile.name = "Bomb";
+
+        }
+
+        public override void Kill(int timeLeft)
+        {
+
+            Vector2 position = projectile.Center;
+            Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
+            int radius = 20;     //this is the explosion radius, the highter is the value the bigger is the explosion
+
+            for (int x = -radius; x <= radius; x++)
+            {
+                for (int y = -radius; y <= radius; y++)
+                {
+                    int xPosition = (int)(x + position.X / 10.0f);
+                    int yPosition = (int)(y + position.Y / 10.0f);
+
+                    if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
+                    {
+                        WorldGen.KillTile(xPosition, yPosition, false, false, false);  //this make the explosion destroy tiles  
+                        Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);  //this is the dust that will spawn after the explosion
+                    }
+                }
+            }
+
+        }
+    }
+}
