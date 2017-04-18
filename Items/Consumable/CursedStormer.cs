8c7a40be976e73ed7968ddf7f2c0da848ff4135d
Zekrom <ibrahimkhawaja08@gmail.com>
2017-04-18 15:20:14 -0500

    XD! Hamboys family is a joke

---
+++ Items/Consumable/CursedStormer.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Events;
+
+namespace ForgottenMemories.Items.Consumable
+{
+    public class CursedStormer : ModItem
+    {
+        public override void SetDefaults()
+        {
+            use.style = 4;
+
+        }
+    } 
+}
