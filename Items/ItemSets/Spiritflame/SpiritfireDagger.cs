756a36389e5ccadbb618d915544950d67754e20f
ColinAV516 <colinav516@gmail.com>
2017-07-24 19:24:02 -0700

    god church

--- Items/ItemSets/Spiritflame/SpiritfireDagger.cs
+++ Items/ItemSets/Spiritflame/SpiritfireDagger.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.ItemSets.Spiritflame
 		public override void SetDefaults()
 		{
 
-			item.damage = 36;
+			item.damage = 41;
 			item.thrown = true;
 			item.noMelee = true;
 			item.noUseGraphic = true;
