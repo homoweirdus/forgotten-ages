9c2ecf5f86e07fa69edb02251c2de9453dda6edc
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-04 02:39:44 +0100

    Added a second tooltip

--- Items/Ranged/Artemis.cs
+++ Items/Ranged/Artemis.cs
@@ -36,7 +36,7 @@ namespace ForgottenMemories.Items.Ranged
     public override void SetStaticDefaults()
     {
       DisplayName.SetDefault("Artemis");
-      Tooltip.SetDefault("Fires a night arrow that splits into stars");
+      Tooltip.SetDefault("Fires a night arrow that splits into stars"\nThe Envoy Of The Darkness);
     }
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
