e1fc2399317959c57452f8f30423d2d9a33bc60b
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-04 15:02:19 +0100

    Added minions

---
+++ Items/Melee/Fluctuate.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class Fluctuate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			ItemID.Sets.Yoyo[item.type] = true;
+			ItemID.Sets.GamepadExtraRange[item.type] = 15;
+			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.useStyle = 5;
+			item.width = 24;
+			item.height = 24;
+			item.noUseGraphic = true;
+			item.UseSound = SoundID.Item1;
+			item.melee = true;
+			item.channel = true;
+			item.noMelee = true;
+			item.shoot = mod.ProjectileType<Projectiles.FluctuateProj>();
+			item.useAnimation = 25;
+			item.useTime = 25;
+			item.shootSpeed = 8f;
+
+			item.knockBack = 6f;
+			item.damage = 19;
+			item.value = Item.sellPrice(0, 2, 50, 0);
+			item.rare = 2;
+		}
+	}
+}
