48b44a707235c2abe7c0f565214b1a08872d958c
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-05 14:58:09 +0100

    Deviant art

---
+++ Projectiles/HamboyProj.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class HamboyProj : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 16;
+			projectile.height = 16;
+            projectile.aiStyle = 2;
+            projectile.friendly = true;
+            projectile.thrown = true;
+			projectile.timeLeft = 600;
+			projectile.penetrate = -1;
+			projectile.ignoreWater = true;
+			aiType = ProjectileID.BoneGloveProj;
+		}
+	}
+}
