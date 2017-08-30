045d1ff1648193b098e6cb3d4e29e7d9adc5ba13
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-08-30 01:52:54 +0100

    meme fix

---
+++ Items/Hook/BrassHook.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.Hook
+{
+	class BrassHook : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Brass Hook");
+		}
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "BrassAlloy", 10);
+                      recipe.AddTile(16);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.AmethystHook);
+			item.shootSpeed = 50f;
+			item.shoot = mod.ProjectileType("BrassProjectile");
+		}
+	}
+	class BrassProjectile : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("${ProjectileName.GemHookAmethyst}");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.GemHookAmethyst);
+		}
+		public override float GrappleRange()
+		{
+			return 250f;
+		}
+
+		public override void NumGrappleHooks(Player player, ref int numHooks)
+		{
+			numHooks = 2;
+		}
+
+		public override void GrappleRetreatSpeed(Player player, ref float speed)
+		{
+			speed = 5f;
+		}
+
+		public override void GrapplePullSpeed(Player player, ref float speed)
+		{
+			speed = 15;
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Vector2 playerCenter = Main.player[projectile.owner].MountedCenter;
+			Vector2 center = projectile.Center;
+			Vector2 distToProj = playerCenter - projectile.Center;
+			float projRotation = distToProj.ToRotation() - 1.57f;
+			float distance = distToProj.Length();
+			while (distance > 30f && !float.IsNaN(distance))
+			{
+				distToProj.Normalize();             
+				distToProj *= 24f;               
+				center += distToProj;                 
+				distToProj = playerCenter - center;    
+				distance = distToProj.Length();
+				Color drawColor = lightColor;
+
+				spriteBatch.Draw(mod.GetTexture("Items/Hook/BrassChain"), new Vector2(center.X - Main.screenPosition.X, center.Y - Main.screenPosition.Y),
+					new Rectangle(0, 0, Main.chain30Texture.Width, Main.chain30Texture.Height), drawColor, projRotation,
+					new Vector2(Main.chain30Texture.Width * 0.5f, Main.chain30Texture.Height * 0.5f), 1f, SpriteEffects.None, 0f);
+			}
+			return true;
+		}
+	}
+}
