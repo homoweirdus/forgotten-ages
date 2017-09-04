e5f5832ed6c20c56044168813bfccb512fc7c51f
ColinAV516 <colinav516@gmail.com>
2017-09-04 13:58:55 -0700

    Be more careful next time five

--- Items/Melee/Gourd.cs
+++ Items/Melee/Gourd.cs
@@ -4,33 +4,35 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ForgottenMemories.Items.Melee {
-public class Gourd : ModItem
+namespace ForgottenMemories.Items.Melee 
 {
-    public override void SetDefaults()
-    {
+	public class Gourd : ModItem
+	{
+		public override void SetDefaults()
+		{
 
-        item.useStyle = 5;
-        item.width = 24;
-        item.height = 24;
-        item.noUseGraphic = true;
-        item.UseSound = SoundID.Item1;
-        item.melee = true;
-        item.channel = true;
-        item.noMelee = true;
-        item.useAnimation = 25;
-        item.useTime = 25;
-        item.shootSpeed = 16f;
-        item.knockBack = 3.75f;
-        item.damage = 16;
-        item.value = Item.sellPrice(0, 0, 50, 0);
-        item.rare = 3;
-        item.shoot = mod.ProjectileType("GourdProj");
-    }
+			item.useStyle = 5;
+			item.width = 24;
+			item.height = 24;
+			item.noUseGraphic = true;
+			item.UseSound = SoundID.Item1;
+			item.melee = true;
+			item.channel = true;
+			item.noMelee = true;
+			item.useAnimation = 25;
+			item.useTime = 25;
+			item.shootSpeed = 16f;
+			item.knockBack = 3.75f;
+			item.damage = 16;
+			item.value = Item.sellPrice(0, 0, 50, 0);
+			item.rare = 3;
+			item.shoot = mod.ProjectileType("GourdProj");
+		}
 
-    public override void SetStaticDefaults()
-    {
-      DisplayName.SetDefault("Cackle");
-      Tooltip.SetDefault("");
-    }
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cackle");
+			Tooltip.SetDefault("");
+		}
+	}
 }
