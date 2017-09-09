cfb28a42a71fa9902c28567adbba9d973d87127c
jesusonwheels <31602665+jesusonwheels@users.noreply.github.com>
2017-09-09 12:33:49 -0500

    k

--- Items/Melee/Elysian.cs
+++ Items/Melee/Elysian.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.Melee
 			ItemID.Sets.Yoyo[item.type] = true;
 			ItemID.Sets.GamepadExtraRange[item.type] = 15;
 			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
-			Tooltip.SetDefault("-Heaven Yoyo-");
+			Tooltip.SetDefault("Made with pure angel dust!");
 		}
 
 		public override void SetDefaults()
