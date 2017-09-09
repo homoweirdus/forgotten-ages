37a432b84bd806b684474baf7bc568781306c279
jesusonwheels <31602665+jesusonwheels@users.noreply.github.com>
2017-09-09 12:50:58 -0500

    bob

--- Items/Melee/Sediment.cs
+++ Items/Melee/Sediment.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.Melee
 			ItemID.Sets.Yoyo[item.type] = true;
 			ItemID.Sets.GamepadExtraRange[item.type] = 15;
 			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
-			Tooltip.SetDefault("Used to start the 'Heaven' progression");
+			Tooltip.SetDefault("Only the beginning");
 		}
 
 		public override void SetDefaults()
