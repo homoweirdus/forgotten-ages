18cded2924d18ec797c793c40d71fd0c14cab9af
jesusonwheels <31602665+jesusonwheels@users.noreply.github.com>
2017-09-09 12:52:01 -0500

    k

--- Items/Melee/Valhalla.cs
+++ Items/Melee/Valhalla.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.Melee
 			ItemID.Sets.Yoyo[item.type] = true;
 			ItemID.Sets.GamepadExtraRange[item.type] = 15;
 			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
-			Tooltip.SetDefault("-Heaven Yoyo-");
+			Tooltip.SetDefault("Heaven's wrath..... yoyoified!");
 		}
 
 		public override void SetDefaults()
