35ede184735e8bfd222c1053fcc1d0297bbf0eaa
ColinAV516 <colinav516@gmail.com>
2017-09-09 14:01:18 -0700

    olympus

--- Projectiles/Bazaar/OlympusProj.cs
+++ Projectiles/Bazaar/OlympusProj.cs
@@ -3,11 +3,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Collections.Generic;
+using System;
 
 namespace ForgottenMemories.Projectiles.Bazaar
 {
 	public class OlympusProj : ModProjectile
 	{
+		int timer = 0;
 		public override void SetStaticDefaults()
 		{
 			ProjectileID.Sets.YoyosLifeTimeMultiplier[projectile.type] = 7.5f;
@@ -26,10 +29,35 @@ namespace ForgottenMemories.Projectiles.Bazaar
 			projectile.melee = true;
 			projectile.scale = 1f;
 		}
-		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
-        {
-			target.AddBuff(BuffID.Midas,	120);
-			target.AddBuff(BuffID.Shine,	120);			
+		
+		public override void AI()
+		{
+			Vector2 move = Vector2.Zero;
+			float distance = 100f;
+			bool target = false;
+			for (int k = 0; k < 200; k++)
+			{
+				if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+				{
+					Vector2 newMove = Main.npc[k].Center - projectile.Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						newMove.Normalize();
+						move = newMove;
+						distance = distanceTo;
+						target = true;
+					}
+				}
+			}
+			timer++;
+			if (target && timer >= 50)
+			{
+				int proj = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, move.X * 8f, move.Y * 8f, mod.ProjectileType("LightningChain"), projectile.damage / 2, 5f, projectile.owner);
+				Main.projectile[proj].melee = true;
+				Main.projectile[proj].ranged = false;
+				timer = 0;
+			}
 		}
 	}
 }
