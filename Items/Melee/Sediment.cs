669959987c3bbe54bc9b4b7bc53ae24678ba77f3
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-09-09 22:35:20 +0100

    Use quotation marks around quotes thanks

--- Items/Melee/Sediment.cs
+++ Items/Melee/Sediment.cs
@@ -11,7 +11,7 @@ namespace ForgottenMemories.Items.Melee
 			ItemID.Sets.Yoyo[item.type] = true;
 			ItemID.Sets.GamepadExtraRange[item.type] = 15;
 			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
-			Tooltip.SetDefault("Sandy and wet to the touch");
+			Tooltip.SetDefault("'An odd mixture of the world's mystical rocks'");
 		}
 
 		public override void SetDefaults()
