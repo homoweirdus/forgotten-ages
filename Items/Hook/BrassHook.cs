3fb1c60a511e4d883c013c63292810be0b1cea06
Paradiscal <30707639+Paradiscal@users.noreply.github.com>
2017-09-09 21:43:45 +0100

    This better fix it

--- Items/Hook/BrassHook.cs
+++ Items/Hook/BrassHook.cs
@@ -52,7 +52,7 @@ namespace ForgottenMemories.Items.Hook
 
 		public override void GrappleRetreatSpeed(Player player, ref float speed)
 		{
-			speed = 12f;
+			speed = 22f;
 		}
 
 		public override void GrapplePullSpeed(Player player, ref float speed)
