b3555a569e5bd033dc12511c2781429455ed730a
ColinAV516 <colinav516@gmail.com>
2017-09-16 11:51:50 -0700

    explosion fixes

--- Items/Ammo/PurpleDart.cs
+++ Items/Ammo/PurpleDart.cs
@@ -4,43 +4,44 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 
-namespace ForgottenMemories.Items.Ammo {
-public class PurpleDart : ModItem
+namespace ForgottenMemories.Items.Ammo 
 {
-	
-    public override void SetDefaults()
-    {
+	public class PurpleDart : ModItem
+	{
+		
+		public override void SetDefaults()
+		{
 
-        item.damage = 5;
-        item.ranged = true;
-        item.width = 22;
-        item.height = 22;
+			item.damage = 10;
+			item.ranged = true;
+			item.width = 22;
+			item.height = 22;
 
-		item.shootSpeed = 2f;
-		item.shoot = mod.ProjectileType("PurpleDartProj");
-        item.knockBack = 1;
-		item.UseSound = SoundID.Item1;
-		item.scale = 1f;
-        item.value = 3;
-        item.rare = 1;
-		item.consumable = true;
-		item.maxStack = 999;
-        item.ammo = AmmoID.Dart;
-	}
+			item.shootSpeed = 2f;
+			item.shoot = mod.ProjectileType("PurpleDartProj");
+			item.knockBack = 1;
+			item.UseSound = SoundID.Item1;
+			item.scale = 1f;
+			item.value = 3;
+			item.rare = 1;
+			item.consumable = true;
+			item.maxStack = 999;
+			item.ammo = AmmoID.Dart;
+		}
 
-    public override void SetStaticDefaults()
-    {
-      DisplayName.SetDefault("Purple Dart");
-      Tooltip.SetDefault("Shot out of blowpipes, explodes into yellow fire on contact");
-    }
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Purple Dart");
+			Tooltip.SetDefault("Shot out of blowpipes, explodes into yellow fire on contact");
+		}
 
 		
 		public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(null, "DarkSludge", 1);
-            recipe.SetResult(this, 200);
-            recipe.AddRecipe();
-        }
-    }
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "DarkSludge", 1);
+			recipe.SetResult(this, 200);
+			recipe.AddRecipe();
+		}
+	}
 }
--- Projectiles/PurpleDartProj.cs
+++ Projectiles/PurpleDartProj.cs
@@ -4,29 +4,35 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 
-namespace ForgottenMemories.Projectiles {
-    public class PurpleDartProj : ModProjectile
+namespace ForgottenMemories.Projectiles
 {
-	public override void SetDefaults()
+	public class PurpleDartProj : ModProjectile
 	{
-		projectile.width = 5;
-		projectile.height = 7;
-		projectile.aiStyle = 1;
-		projectile.penetrate = 1;
-		projectile.ranged = true;
-		projectile.friendly = true;
-        projectile.timeLeft = 3000;
-	}
-	
-	public override void SetStaticDefaults()
+		public override void SetDefaults()
+		{
+			projectile.width = 5;
+			projectile.height = 7;
+			projectile.aiStyle = 1;
+			projectile.penetrate = 1;
+			projectile.ranged = true;
+			projectile.friendly = true;
+			projectile.timeLeft = 3000;
+		}
+		
+		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Purple Dart");
 		}
-	
-	    public override void Kill(int timeLeft)
-{
-    Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0f, 0f, mod.ProjectileType("PurpleDartBoom"), projectile.damage, 0f, projectile.owner, 0f, 0f);
-    Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 34);
-}
-}
+		
+		public override void Kill(int timeLeft)
+		{
+			Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0f, 0f, mod.ProjectileType("PurpleDartBoom"), projectile.damage, 0f, projectile.owner, 0f, 0f);
+			Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 34);
+		}
+		
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{	
+			target.immune[projectile.owner] = 5;
+		}
+	}
 }	
