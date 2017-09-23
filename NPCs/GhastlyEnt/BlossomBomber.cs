e473f428e2c94faa2f8cb2fa2d62bc46e491d0d1
ColinAV516 <colinav516@gmail.com>
2017-09-23 09:24:20 -0700

    you need a high iq to understand rick and morty

---
+++ NPCs/GhastlyEnt/BlossomBomber.cs
@@ -0,0 +1,117 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Events;
+using Terraria.ModLoader;
+using System;
+
+namespace ForgottenMemories.NPCs.GhastlyEnt
+{
+	public class BlossomBomber : ModNPC
+	{
+		int timer = 0;
+		int timer2 = 0;
+		bool hasShot = false;
+		public override void SetDefaults()
+		{
+			npc.width = 74;
+			npc.height = 70;
+			npc.damage = 28;
+			npc.defense = 12;
+			npc.lifeMax = 80;
+			npc.HitSound = SoundID.NPCHit7;
+			npc.DeathSound = SoundID.NPCDeath3;
+			npc.value = 60f;
+			npc.knockBackResist = 0.5f;
+			npc.aiStyle = 3;
+			aiType = 508;
+		}
+		
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Blossom Bomber");
+			Main.npcFrameCount[npc.type] = 9;
+		}
+		
+		public override void AI()
+        {
+			Player player = Main.player[npc.target];
+			npc.TargetClosest(true);
+			timer++;
+			float distance = 300f;
+			Vector2 newMove = npc.Center - player.Center;
+			float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+			
+			if (timer >= 120 && npc.velocity.Y == 0f && distanceTo < distance && !player.dead)
+			{
+				Vector2 vel = (player.Center - npc.Center);
+				vel.Normalize();
+				vel *= 4;
+				vel.X *= 2f;
+				vel.Y -= 6f;
+				Projectile projectile = Main.projectile[Projectile.NewProjectile(npc.Center, vel, mod.ProjectileType("wooballcherry"), (int)(npc.damage/4), 0, Main.myPlayer, 0, 0)];
+				projectile.friendly = false;
+				projectile.hostile = true;
+				hasShot = true;
+				timer = 0;
+			}
+			
+			if (hasShot)
+			{
+				timer2++;
+				npc.velocity.Y = 10f;
+				npc.velocity.X = 0;
+				if (timer2 >= 15)
+				{
+					hasShot = false;
+					timer2 = 0;
+				}
+			}
+		}
+		
+		public override void FindFrame(int frameHeight)
+		{
+			int num1 = 1;
+			if (npc.velocity.Y == 0.0)
+			{
+				if (npc.direction == 1)
+					npc.spriteDirection = 1;
+				if (npc.direction == -1)
+					npc.spriteDirection = -1;
+			}
+			if (hasShot)
+			{
+				npc.frameCounter += 0.2f;
+				npc.frameCounter %= 5; 
+				int frame = (int)npc.frameCounter + 4;
+				npc.frame.Y = frame * frameHeight;
+			}
+			else
+			{
+				npc.frameCounter += 0.25f; 
+				npc.frameCounter %= 4; 
+				int frame = (int)npc.frameCounter; 
+				npc.frame.Y = frame * frameHeight;
+			}
+		}
+		
+		public override void NPCLoot()
+		{
+			
+			for (int m = 0; m <= 5; m++)
+			{
+				int dust = Dust.NewDust(npc.position, npc.width, npc.height, 6);
+			}
+			
+			for (int m = 0; m <= 10; m++)
+			{
+				int dust = Dust.NewDust(npc.position, npc.width, npc.height, 191);
+			}
+					
+			if(Main.rand.Next(20) == 0)
+			{
+				//Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("LivingTwig"));
+			}
+		}
+	}
+}
---
+++ Projectiles/wooballcherry.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using System;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class wooballcherry : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 36;
+			projectile.height = 36;
+			projectile.aiStyle = 2;
+			projectile.hostile = true;
+			projectile.penetrate = 1;
+			Main.projFrames[projectile.type] = 2;
+		}
+		
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cherry Bomb");
+		}
+		
+		public override void AI()
+		{
+			projectile.frameCounter++;
+			if (projectile.frameCounter >= 4)
+			{
+				projectile.frameCounter = 0;
+				projectile.frame = (projectile.frame + 1) % 2;
+			} 
+		}
+		
+		public override void Kill(int timeLeft)
+		{
+			Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0f, 0f, mod.ProjectileType("wooboom"), projectile.damage, 0f, projectile.owner, 0f, 0f);
+			Main.PlaySound(SoundID.Item89, projectile.position);
+		}
+	}
+}
