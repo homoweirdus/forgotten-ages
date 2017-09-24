285a437b68f8e7111656e1ffa9229603f7b8ee51
Paradiscal <Paradiscal@outlook.com>
2017-09-24 22:26:07 +0100

    me

--- Sounds/Sonic.cs
+++ Sounds/Sonic.cs
@@ -2,13 +2,12 @@ using Microsoft.Xna.Framework.Audio;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace ForgottenMemories.Sounds.Sound
+namespace ForgottenMemories.Sounds
 {
 	public class Sonic : ModSound
 	{
 		public override SoundEffectInstance PlaySound(ref SoundEffectInstance soundInstance, float volume, float pan, SoundType type)
 		{
-			// By creating a new instance, this ModSound allows for overlapping sounds. Non-ModSound behavior is to restart the sound, only permitting 1 instance.
 			soundInstance = sound.CreateInstance();
 			soundInstance.Volume = volume * .5f;
 			soundInstance.Pan = pan;
