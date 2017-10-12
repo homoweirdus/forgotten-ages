d886a19a60677cc3cb9e598b230bcbc01892f367
ColinAV516 <colinav516@gmail.com>
2017-10-12 16:55:10 -0700

    frik

--- Projectiles/ForbiddenStaffProj.cs
+++ Projectiles/ForbiddenStaffProj.cs
@@ -10,15 +10,17 @@ namespace ForgottenMemories.Projectiles
 {
 	public class ForbiddenStaffProj : ModProjectile
 	{
+		Vector2 gayvector = new Vector2(0f, -6f);
+		float frick = 2f;
+		bool reverse;
 		public override void SetDefaults()
 		{
-			projectile.width = 10;
-			projectile.height = 10;
+			projectile.width = 18;
+			projectile.height = 18;
 			projectile.aiStyle = -1;
 			projectile.friendly = true;
 			projectile.magic = true;
 			projectile.penetrate = 1;
-			projectile.timeLeft = 200;
 			projectile.alpha = 255;
 		}
 		
@@ -30,25 +32,49 @@ namespace ForgottenMemories.Projectiles
 		public override void AI()
 		{
 			projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
-			if (Main.rand.Next(2) == 0)
+		}
+		
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{	
+			target.immune[projectile.owner] = 13;
+		}
+		
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Microsoft.Xna.Framework.Color color25 = Color.White;
+			Texture2D texture2D3 = Main.projectileTexture[projectile.type];
+			int num156 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type];
+			int y3 = num156 * projectile.frame;
+			Microsoft.Xna.Framework.Rectangle rectangle = new Microsoft.Xna.Framework.Rectangle(0, y3, texture2D3.Width, num156);
+			Vector2 origin2 = rectangle.Size() / 2f;
+			if (!reverse)
 			{
-				int dust;
-				dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 32, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
-				Main.dust[dust].scale = 1.5f;
-				Main.dust[dust].noGravity = true;
+				frick += 0.1f;
 			}
-			if (Main.rand.Next(2) == 0)
+			else 
 			{
-				int dust2;
-				dust2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 57, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
-				Main.dust[dust2].scale = 1.5f;
-				Main.dust[dust2].noGravity = true;
+				frick -= 0.1f;
 			}
+			if (frick >= 5f)
+			{
+				reverse = true;
+			}
+			if (frick <= 1.9f)
+			{
+				reverse = false;
+			}
+			for (int i = 0; i < 8; ++i)
+			{
+				Main.spriteBatch.Draw(mod.GetTexture("Projectiles/ForbiddenStaffProj2"), projectile.position + (-gayvector.RotatedBy(MathHelper.ToRadians(45 * i)) * frick) + projectile.Size / 2f - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color25, projectile.rotation, origin2, 1f, SpriteEffects.None, 0f);
+			}
+			Main.spriteBatch.Draw(Main.projectileTexture[projectile.type], projectile.position + projectile.Size / 2f - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Color.White, projectile.rotation, origin2, projectile.scale, SpriteEffects.None, 0f);
+			gayvector = gayvector.RotatedBy(System.Math.PI / 35);
+			return false;
 		}
 		
 		public override void Kill(int timeLeft)
 		{
-			Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, mod.ProjectileType("forbiddenboom"), projectile.damage, 5f, projectile.owner);
+			Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, mod.ProjectileType("Forboom"), projectile.damage, 5f, projectile.owner);
 			Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 62);
 		}
 	}
---
+++ Projectiles/Forboom.cs
@@ -0,0 +1,52 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Projectiles
+{
+	public class Forboom : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 98;
+			projectile.height = 98;
+			projectile.aiStyle = -1;
+			projectile.friendly = true;
+			projectile.magic = true;
+			projectile.penetrate = 3;
+			projectile.timeLeft = 12;
+			projectile.light = 0.5f;
+			projectile.tileCollide = false;
+			Main.projFrames[projectile.type] = 7;
+			projectile.scale = 1.25f;
+		}
+		
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Forbidden Explosion");
+		}
+		
+		public override void AI()
+		{
+			projectile.frameCounter++;
+			if (projectile.frameCounter >= 2)
+			{
+				projectile.frameCounter = 0;
+				projectile.frame = (projectile.frame + 1) % 7;
+			} 
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Texture2D texture2D3 = Main.projectileTexture[projectile.type];
+			int num156 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type];
+			int y3 = num156 * projectile.frame;
+			Microsoft.Xna.Framework.Rectangle rectangle = new Microsoft.Xna.Framework.Rectangle(0, y3, texture2D3.Width, num156);
+			Vector2 origin2 = rectangle.Size() / 2f;
+			Main.spriteBatch.Draw(Main.projectileTexture[projectile.type], projectile.position + projectile.Size / 2f - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Color.White, projectile.rotation, origin2, projectile.scale, SpriteEffects.None, 0f);
+			return false;
+		}
+	}
+}
