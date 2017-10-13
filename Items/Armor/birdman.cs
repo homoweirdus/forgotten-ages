cbb9c3d5c769a4204bee1c46236522d0ce4d7f4e
Paradiscal <Paradiscal@outlook.com>
2017-10-13 21:35:08 +0100

    kbro

---
+++ Items/Armor/birdman.cs
@@ -0,0 +1,31 @@
+using Terraria.ModLoader;
+
+namespace ForgottenMemories.Items.Armor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class birdman : ModItem
+	{
+	    public override void SetStaticDefaults()
+		{
+		DisplayName.SetDefault("Magnoliac Mask");
+        Tooltip.SetDefault("");
+		}		
+		
+		public override void SetDefaults()
+		{
+			item.width = 32;
+			item.height = 32;
+			item.rare = 1;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead()
+		{
+            return true;   
+        }
+            public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+        {
+            drawHair = false;
+		}
+	}
+}
