ce452422f4c90eeaaf483cfa21f6d6883dc8706b
Paradiscal <Paradiscal@outlook.com>
2017-10-15 10:04:16 +0100

    kbro

---
+++ Tiles/MagnoliacTrophy.cs
@@ -0,0 +1,26 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ForgottenMemories.Tiles
+{
+	public class MagnoliacTrophy : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleWrapLimit = 36;
+			TileObjectData.addTile(Type);
+			dustType = 7;
+			disableSmartCursor = true;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Magnoliac Trophy");
+			AddMapEntry(new Color(120, 85, 60), name);
+			drop = mod.ItemType("MagnoliacTrophy");
+		}
+	}
+}
